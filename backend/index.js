3d11591b090d945ffe4029c89d468b0625bd84bd
tana-sudo <mutandwatanatswa1@gmail.com>
2025-10-26 23:09:26 +0200

    Update server configuration to use environment variable for port and add dotenv setup

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
-const port = 3000 ;
+const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(express.json());
 
@@ -12,6 +14,11 @@ app.get('/data', (req, res) => {
     res.status(200).send('Data received');
 });
 
+app.post('/inserdata', (req, res) => {
+    const data = "tanatswa";
+    console.log('Received data:', data);
+    res.status(200).send('Data received');
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
