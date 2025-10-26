8a24400edce9b62c8d2d2a115c95b3f42785d3a0
tana-sudo <mutandwatanatswa1@gmail.com>
2025-10-26 23:00:47 +0200

    Refactor server setup by removing dotenv and updating data endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
+import bodyParser from 'body-parser';
 const app = express();
 
-const port = process.env.PORT ;
+const port = 3000 ;
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.get('/data', (req, res) => {
+    const data = "Sample data from server";
+    console.log('Received data:', data);
+    res.status(200).send('Data received');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
